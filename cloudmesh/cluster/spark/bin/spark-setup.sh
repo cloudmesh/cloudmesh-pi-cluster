b4f3ddfd3a6c0955738440141e32e71f8eac7be7
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-04-26 15:56:28 -0400

    Updated Spark batch shell files

--- cloudmesh/cluster/spark/bin/spark-setup.sh
+++ cloudmesh/cluster/spark/bin/spark-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd
 sudo apt-get install scala
 sudo wget http://apache.osuosl.org/spark/spark-2.3.4/spark-2.3.4-bin-hadoop2.7.tgz -O sparkout2-3-4.tgz
 sudo tar -xzf sparkout2-3-4.tgz
 cat ~/.bashrc ~/spark-bashrc.txt > ~/temp-bashrc
 sudo cp ~/temp-bashrc ~/.bashrc
+sudo rm ~/temp-bashrc
