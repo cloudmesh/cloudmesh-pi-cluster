c2571ffeb3e8d6a52946c758520287b2ef137659
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-04-26 19:15:03 -0400

    Updated Spark batch shell files

--- cloudmesh/cluster/spark/bin/spark-scp-setup-to-worker.sh
+++ cloudmesh/cluster/spark/bin/spark-scp-setup-to-worker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 scp /bin/spark-setup.sh pi@green001:
 scp ~/spark-bashrc.txt pi@green001:
+scp /bin/spark-uninstall.sh pi@green001:
 ssh pi@green001 sh ~/spark-setup.sh
--- cloudmesh/cluster/spark/bin/spark-setup.sh
+++ cloudmesh/cluster/spark/bin/spark-setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+sudo apt-get install openjdk-8-jre
 sudo apt-get install scala
+cd ~
 sudo wget http://apache.osuosl.org/spark/spark-2.3.4/spark-2.3.4-bin-hadoop2.7.tgz -O sparkout2-3-4.tgz
 sudo tar -xzf sparkout2-3-4.tgz
 cat ~/.bashrc ~/spark-bashrc.txt > ~/temp-bashrc
+sudo cp ~/.bashrc ~/.bashrc-backup
 sudo cp ~/temp-bashrc ~/.bashrc
 sudo rm ~/temp-bashrc
