7e86cae7ff5e5cc7bf15bc1a0b018fab1aa4e662
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-04-27 00:01:06 -0400

    Updated Spark batch shell files

--- cloudmesh/cluster/spark/bin/spark-scp-setup-to-worker.sh
+++ cloudmesh/cluster/spark/bin/spark-scp-setup-to-worker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 scp ~/spark-bashrc.txt pi@green001:
-scp /bin/spark-uninstall.sh pi@green001:
+scp /bin/spark-uninstall2.4.5.sh pi@green001:
 scp ~/sparkout.tgz pi@green001:
 ssh pi@green001 sh ~/spark-worker-setup.sh
-cat >> $SPARK_HOME/conf/slaves << EOF
+sudo cat >> $SPARK_HOME/conf/slaves << EOF
 pi@green001
 EOF
