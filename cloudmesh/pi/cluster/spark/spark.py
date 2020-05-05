18332b2d8eb6912dcf4aa232ae1f75c92a8da319
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-05-05 17:04:39 -0400

    Added pprint into spark.py for debugging

--- cloudmesh/pi/cluster/spark/spark.py
+++ cloudmesh/pi/cluster/spark/spark.py
@@ -165,7 +165,7 @@ class Spark(Installer):
 
     def run_script(self, name=None, hosts=None):
         banner(name)
-        pprint(self.script[name])
+        pprint(self.script['spark.setup'])
         results = self.run(script=self.script[name], hosts=hosts, verbose=True)
 
     def setup(self, master=None, hosts=None):
