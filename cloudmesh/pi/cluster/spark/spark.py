6d76e30d7a98e8a42609e73587574effa85ab807
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-05-07 15:25:16 -0400

    correcting empty files from being created on master

--- cloudmesh/pi/cluster/spark/spark.py
+++ cloudmesh/pi/cluster/spark/spark.py
@@ -299,9 +299,19 @@ class Spark:
             print(script)
         else:
             f = open("/home/pi/spark-setup-worker.sh", "w+")
-            # f.write("test")
+            f.write("""
+                sudo apt-get update
+                sudo apt-get install default-jdk
+                sudo apt-get install scala
+                cd ~
+                sudo tar -xzf sparkout.tgz
+                cat ~/.bashrc ~/spark-bashrc.txt > ~/temp-bashrc
+                sudo cp ~/.bashrc ~/.bashrc-backup
+                sudo cp ~/temp-bashrc ~/.bashrc
+                sudo rm ~/temp-bashrc
+                sudo chmod 777 ~/spark-2.4.5-bin-hadoop2.7/""")
             f.close()
-            Installer.add_script("~/spark-setup-worker.sh", script)
+            #Installer.add_script("~/spark-setup-worker.sh", script)
 
     def create_spark_bashrc_txt(self):
         banner("Creating the spark-bashrc.txt file")
@@ -310,7 +320,15 @@ class Spark:
             print(script)
         else:
             f = open("/home/pi/spark-bashrc.txt", "w+")
-            # f.write("test")
+            f.write(""" 
+                #JAVA_HOME
+                export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-armhf/
+                #SCALA_HOME
+                export SCALA_HOME=/usr/share/scala
+                export PATH=$PATH:$SCALA_HOME/bin
+                #SPARK_HOME
+                export SPARK_HOME=~/spark-2.4.5-bin-hadoop2.7
+                export PATH=$PATH:$SPARK_HOME/bintest""")
             f.close()
-            Installer.add_script("~/spark-bashrc.txt", script)
+            #Installer.add_script("~/spark-bashrc.txt", script)
         # spark-bashrc.txt is being created but it's empty
