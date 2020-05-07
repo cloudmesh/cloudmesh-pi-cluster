b0de9daba98511b1c5b517cd9c1fcd56e086fe58
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-05-07 17:56:01 -0400

    updating spark.py

--- cloudmesh/pi/cluster/spark/bin/spark-master-bashrc.sh
+++ cloudmesh/pi/cluster/spark/bin/spark-master-bashrc.sh
@@ -6,5 +6,5 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-armhf/
 export SCALA_HOME=/usr/share/scala
 #SPARK_HOME
 export SPARK_HOME=~/spark-2.4.5-bin-hadoop2.7
-export PATH=/home/pi/ENV3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:$SPARK_HOME/bin:$SCALA_HOME/bin
+export PATH=/home/pi/ENV3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$SPARK_HOME/bin:$SCALA_HOME/bin
 EOF
