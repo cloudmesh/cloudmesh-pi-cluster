42a9feb4a54037b599ac2e1fb66fdf965a168a13
Gregor von Laszewski <laszewski@gmail.com>
2021-03-25 19:04:55 -0400

    switch master to manager

--- cloudmesh/pi/cluster/k3/README.md
+++ cloudmesh/pi/cluster/k3/README.md
@@ -1,14 +1,14 @@
 # Install
 
 You must first set up your workers to connect to the internet through
-your master. This is done by using the command ```cms bridge```. You can
+your manager. This is done by using the command ```cms bridge```. You can
 find the documentation here to set it up:
-<https://github.com/cloudmesh/cloudmesh-pi-cluster/tree/master/cloudmesh/bridge>
+<https://github.com/cloudmesh/cloudmesh-pi-cluster/tree/main/cloudmesh/bridge>
 
-This command will install Kubernetes to the master and workers you provide, and will join the workers to your master's cluster.
+This command will install Kubernetes to the manager and workers you provide, and will join the workers to your manager's cluster.
 
 ```
-master$ cms pi k3 install [--master=MASTER] [--workers=WORKERS]
+manager$ cms pi k3 install [--manager=MANAGER] [--workers=WORKERS]
 ```
 
 **Note**: If you have never enabled containers on your raspberry pis
@@ -18,40 +18,40 @@ master$ cms pi k3 install [--master=MASTER] [--workers=WORKERS]
 
 Enabling Containers: For Kubernetes to work, you must enable containers.
 To do this, append the following command to the install command above to
-enable containers on the master and workers you list.
+enable containers on the manager and workers you list.
 
 ```
-master$ cms pi k3 install [--master=MASTER] [--workers=WORKERS] --step=enable_containers
+manager$ cms pi k3 install [--manager=MANAGER] [--workers=WORKERS] --step=enable_containers
 ```
 
 ## Other Available Commands
 
-Join a worker to the master's cluster if Kubernetes is already installed on the node 
+Join a worker to the manager's cluster if Kubernetes is already installed on the node 
 
 ```
-master$ cms pi k3 join --workers=WORKERS
+manager$ cms pi k3 join --workers=WORKERS
 ```
 
-Uninstall Kubernetes on either the master or any worker
+Uninstall Kubernetes on either the manager or any worker
 
 ```
-master$ cms pi k3 uninstall [--master=MASTER] [--workers=WORKERS]
+manager$ cms pi k3 uninstall [--manager=MANAGER] [--workers=WORKERS]
 ```
 
-Delete a node(s) from your master's cluster
+Delete a node(s) from your manager's cluster
 
 ```
-master$ cms pi k3 delete [--master=MASTER] [--workers=WORKERS]
+manager$ cms pi k3 delete [--manager=MANAGER] [--workers=WORKERS]
 ```
 
 Run a test on your setup cluster **(Not yet implemented)**
 
 ```
-master$ cms pi k3 test [--master=MASTER] [--workers=WORKERS]
+manager$ cms pi k3 test [--manager=MANAGER] [--workers=WORKERS]
 ```
 
 View details about your cluster:
 
 ```
-master$ cms pi k3 view
+manager$ cms pi k3 view
 ```
--- cloudmesh/pi/cluster/k3/k3.py
+++ cloudmesh/pi/cluster/k3/k3.py
@@ -52,7 +52,7 @@ class Installer:
         return msg.replace("\n", "").replace("  ", " ")
 
     @staticmethod
-    def get_master_ip_address(ifname):
+    def get_manager_ip_address(ifname):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         return socket.inet_ntoa(fcntl.ioctl(
             s.fileno(),
@@ -69,19 +69,19 @@ class K3(Installer):
 
     def execute(self, arguments):
         """
-        pi k3 install [--master=MASTER] [--workers=WORKERS] [--step=COMMAND]
-        pi k3 join --master=MASTER --workers=WORKERS
-        pi k3 uninstall [--master=MASTER] [--workers=WORKERS]
+        pi k3 install [--manager=MANAGER] [--workers=WORKERS] [--step=COMMAND]
+        pi k3 join --manager=MANAGER --workers=WORKERS
+        pi k3 uninstall [--manager=MANAGER] [--workers=WORKERS]
         pi k3 delete --workers=WORKERS
-        pi k3 test [--master=MASTER] [--workers=WORKERS]
+        pi k3 test [--manager=MANAGER] [--workers=WORKERS]
         pi k3 view
         :param arguments:
         :return:
         """
 
-        master = None
-        if arguments.master:
-            master = arguments.master
+        manager = None
+        if arguments.manager:
+            manager = arguments.manager
 
         hosts = None
         if arguments.workers:
@@ -95,35 +95,35 @@ class K3(Installer):
         #    Console.error("You need to specify at least one worker")
         #    return ""
 
-        # if master is None:
-        #    Console.error("You need to specify a master")
+        # if manager is None:
+        #    Console.error("You need to specify a manager")
         #    return ""
 
         if arguments.install:
-            self.install(master, hosts, step)
+            self.install(manager, hosts, step)
 
         if arguments.join:
-            self.join(master, hosts)
+            self.join(manager, hosts)
 
         if arguments.uninstall:
-            self.uninstall(master, hosts)
+            self.uninstall(manager, hosts)
 
         if arguments.delete:
-            self.delete(master, hosts)
+            self.delete(manager, hosts)
 
         if arguments.test:
-            self.test(master, hosts)
+            self.test(manager, hosts)
 
         if arguments.view:
             self.view()
 
     def enable_containers(self, filename="/boot/cmdline.txt", hosts=None):
         line = "cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-        warning = "Your master already has containers enabled"
+        warning = "Your manager already has containers enabled"
         self.add_to_file(filename, line, warning)
 
         if hosts is not None:
-            # Create tmp file on master with enable_containers line
+            # Create tmp file on manager with enable_containers line
             source = "~/container_tmp.txt"
             command = f'echo "{line}" >> {source}'
             os.system(command)
@@ -153,37 +153,37 @@ class K3(Installer):
             jobSet.run()
             # jobSet.Print()
 
-            # Delete tmp file on master
+            # Delete tmp file on manager
             command = f"rm {source}"
             os.system(command)
 
-    def install(self, master=None, hosts=None, step=None):
-        # Setup containers on master
-        if master is None and hosts:
-            Console.error("You must specify a master to set up nodes")
+    def install(self, manager=None, hosts=None, step=None):
+        # Setup containers on manager
+        if manager is None and hosts:
+            Console.error("You must specify a manager to set up nodes")
             raise ValueError
 
         if step is not None:
             if step in 'enable_containers':
                 self.enable_containers(hosts=hosts)
 
-        # Install K3S on the master
-        if master is not None:
+        # Install K3S on the manager
+        if manager is not None:
 
-            if type(master) != list:
-                master = Parameter.expand(master)
+            if type(manager) != list:
+                manager = Parameter.expand(manager)
 
             #
-            # TODO - bug I should be able to run this even if I am not on master
+            # TODO - bug I should be able to run this even if I am not on manager
             #
-            banner(f"Setup Master: {master[0]}")
+            banner(f"Setup Manager: {manager[0]}")
 
             command = Installer.oneline(f"""
             curl -sfL https://get.k3s.io | sh -
             """)
-            jobSet = JobSet("kubernetes_master_install", executor=JobSet.ssh)
+            jobSet = JobSet("kubernetes_manager_install", executor=JobSet.ssh)
             jobSet.add(
-                {"name": self.hostname, "host": master[0], "command": command})
+                {"name": self.hostname, "host": manager[0], "command": command})
             jobSet.run()
             result_stdout = jobSet.array()[0]['stdout'].decode('UTF-8')
             if "No change detected" in result_stdout:
@@ -192,16 +192,16 @@ class K3(Installer):
 
         # Setup workers and join to cluster
         #
-        # TODO - bug I should be able to run this even if I am not on master
+        # TODO - bug I should be able to run this even if I am not on manager
         #
         if hosts is not None:
-            if master is not None:
-                banner(f"Get Join Token From {master[0]}")
+            if manager is not None:
+                banner(f"Get Join Token From {manager[0]}")
                 command = "sudo cat /var/lib/rancher/k3s/server/node-token"
                 jobSet = JobSet("kubernetes_token_retrieval",
                                 executor=JobSet.ssh)
                 jobSet.add(
-                    {"name": master[0], "host": master[0], "command": command})
+                    {"name": manager[0], "host": manager[0], "command": command})
                 jobSet.run()
                 token = jobSet.array()[0]['stdout'].decode('UTF-8')
 
@@ -221,11 +221,11 @@ class K3(Installer):
                 jobSet.run(parallel=len(hosts))
                 jobSet.Print()
 
-                # Join workers to master's cluster
+                # Join workers to manager's cluster
                 # TODO - Currently get ip address from eth0 instead of using
                 #        hostname
-                # because worker does not know master's host name
-                ip = self.get_master_ip_address('eth0')
+                # because worker does not know manager's host name
+                ip = self.get_manager_ip_address('eth0')
 
                 command = f"""sudo k3s agent --server https://{ip}:{self.port} 
                 --token {token}"""
@@ -239,24 +239,24 @@ class K3(Installer):
                 jobSet.Print()
             else:
                 Console.warning(
-                    "You must have the master parameter set to burn workers")
+                    "You must have the manager parameter set to burn workers")
 
         # Print created cluster
         self.view()
 
-    def join(self, master=None, hosts=None):
-        if hosts is not None and master is not None:
-            banner(f"Get Join Token From {master}")
+    def join(self, manager=None, hosts=None):
+        if hosts is not None and manager is not None:
+            banner(f"Get Join Token From {manager}")
             command = "sudo cat /var/lib/rancher/k3s/server/node-token"
             jobSet = JobSet("kubernetes_token_retrieval", executor=JobSet.ssh)
-            jobSet.add({"name": master, "host": master, "command": command})
+            jobSet.add({"name": manager, "host": manager, "command": command})
             jobSet.run()
             token = jobSet.array()[0]['stdout'].decode('UTF-8')
 
             # TODO - Currently get ip address from eth0 instead of using
             #        hostname
-            # because worker does not know master's host name
-            ip = self.get_master_ip_address('eth0')
+            # because worker does not know manager's host name
+            ip = self.get_manager_ip_address('eth0')
 
             command = f"""
             sudo k3s agent --server https://{ip}:{self.port}
@@ -270,14 +270,14 @@ class K3(Installer):
 
             self.view()
 
-    def uninstall(self, master=None, hosts=None):
-        # Uninstall master
-        if master is not None:
-            banner(f"Uninstalling Master {master}")
+    def uninstall(self, manager=None, hosts=None):
+        # Uninstall manager
+        if manager is not None:
+            banner(f"Uninstalling Manager {manager}")
 
             command = "/usr/local/bin/k3s-uninstall.sh"
-            jobSet = JobSet("kubernetes_master_uninstall", executor=JobSet.ssh)
-            jobSet.add({"name": master, "host": master, "command": command})
+            jobSet = JobSet("kubernetes_manager_uninstall", executor=JobSet.ssh)
+            jobSet.add({"name": manager, "host": manager, "command": command})
             jobSet.run()
             jobSet.Print()
 
@@ -300,17 +300,17 @@ class K3(Installer):
             #        order=["name", "command", "status", "stdout",
             #               "returncode"]))
 
-    def delete(self, master=None, hosts=None):
-        # Delete master node
+    def delete(self, manager=None, hosts=None):
+        # Delete manager node
         # TODO - k3s does not allow you to delete it's parent node
-        # if master is not None:
-        #    banner(f"Deleting Master Node: {master}")
+        # if manager is not None:
+        #    banner(f"Deleting Manager Node: {manager}")
         #
         #    command = Installer.oneline(f"""
-        #                sudo kubectl delete {master}
+        #                sudo kubectl delete {manager}
         #    """)
-        #    jobSet = JobSet("kubernetes_master_delete", executor=JobSet.ssh)
-        #    jobSet.add({"name": self.hostname, "host": master,
+        #    jobSet = JobSet("kubernetes_manager_delete", executor=JobSet.ssh)
+        #    jobSet.add({"name": self.hostname, "host": manager,
         #                "command": command})
         #    jobSet.run()
 
@@ -332,7 +332,7 @@ class K3(Installer):
                                             order=["name", "command", "status",
                                                    "stdout", "returncode"]))
 
-    def test(self, master=None, hosts=None):
+    def test(self, manager=None, hosts=None):
         print("Test not yet implemented")
         # TODO - Check for software that is installed or can be installed
         #        to run a test
