abe4e2381babada8aa168372042c13c036618b08
Gregor von Laszewski <laszewski@gmail.com>
2021-04-20 07:33:02 -0400

    add mpi template

---
+++ cloudmesh/pi/cluster/mpi/Mpi.py
@@ -0,0 +1,10 @@
+class Mpi:
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def install(self):
+        raise NotImplementedError
+
+    def run(self):
+        raise  NotImplementedError
