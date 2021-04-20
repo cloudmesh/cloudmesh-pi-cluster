ecfde28e615ac0ecb02c51d8b72a3564017ca7ef
Gregor von Laszewski <laszewski@gmail.com>
2021-04-20 08:40:18 -0400

    start parameter passing fro mpi

--- cloudmesh/pi/cluster/mpi/Mpi.py
+++ cloudmesh/pi/cluster/mpi/Mpi.py
@@ -1,10 +1,18 @@
+from cloudmesh.common.parameter import Parameter
 class Mpi:
-
+"""
+pi mpi install [NAMES]
+pi mpirun [--hosts=HOSTS] --parameters=PARAMETERS] COMMAND
+"""
     def __init__(self):
+        self.hosts = None
         raise NotImplementedError
 
-    def install(self):
+    def install(self, names):
+        self.hosts = Parameter.expand(names)
         raise NotImplementedError
 
-    def run(self):
+    def run(self, hosts=None, parameters=None, command=None):
         raise  NotImplementedError
+
+
