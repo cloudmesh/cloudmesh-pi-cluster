1dd2ab78905e6ff21091a8661d1c11d3c731630a
dkkolli <dhakshesh.kolli@gmail.com>
2021-05-12 00:24:45 -0400

    print intermediate results of functions clearly for share and unshare

--- cloudmesh/pi/nfs/Nfs.py
+++ cloudmesh/pi/nfs/Nfs.py
@@ -31,61 +31,73 @@ class Nfs:
     # mount manager directory to a shared directory, share that directory with workers
     # (shared directory will be created on each pi)
     def share(self, paths, hostnames):
-        # get manager IP
-        manager_ip = Shell.run('hostname -I').split(' ')[1]
+        
+        #for debugging
+        result = {}
+
+        #create new filesystem which will be share point, assign proper owners
+        def _create_share_system(host, path):
+            command = f"sudo mkdir -p {path}"
+            r = Host.ssh(hosts=f"pi@{host}", command=command)
+            result[f"pi@{host}: " + command] = r[0]['success']
+
+            command = f"sudo chown -R pi:pi {path}"
+            r = Host.ssh(hosts=f"pi@{host}", command=command)
+            result[f"pi@{host}: " + command] = r[0]['success']
 
         try:
+            #necessary IPs & hostnames for sharing
+            manager_ip = Shell.run('hostname -I').split(' ')[1]
             mounting, mounting_to = paths.split(',')
-            Console.info(f'Mounting {mounting} to {mounting_to}')
-
             pis = hostnames.split(',')
             manager = pis[0]
             workers = pis[1:]
 
-            # create and bind directory paths on manager
-            Console.info("mkdir /mnt/nfs manager")
-            r = Host.ssh(hosts=f"pi@{manager}", command=f" sudo mkdir -p {mounting_to}")
-            Console.info(r)
-            Console.info("chown /mnt/nfs manager")
-            r = Host.ssh(hosts=f"pi@{manager}", command=f"sudo chown -R pi:pi {mounting_to}")
-            Console.info(r)
-            Console.info("mount bind /mnt/nfs manager")
-            r = Host.ssh(hosts=f"pi@{manager}", command=f"sudo mount --bind {mounting} {mounting_to}")
-            Console.info(r)
-            Console.info("Manager directories bound")
+            # create on manager the share point
+            _create_share_system(manager,mounting_to)
+
+            #bind on manager an existing filesystem to the share point
+            command = f"sudo mount --bind {mounting} {mounting_to}"
+            r = Host.ssh(hosts=f"pi@{manager}", command=command)
+            result[f"pi@{manager}: " + command] = r[0]['success']
 
             # preserve binding after reboot on manager
-            Console.info("edit fstab manager")
             add_to_fstab = f"{mounting}\t{mounting_to}\tnone\tbind\t0\t0"
-            r = Host.ssh(hosts=f"pi@{manager}", command=f"echo \"{add_to_fstab}\" | sudo tee --append /etc/fstab")
-            Console.info(r)
-            Console.info("Binding preserved for reboot")
+            command = f"echo \"{add_to_fstab}\" | sudo tee --append /etc/fstab"
+            r = Host.ssh(hosts=f"pi@{manager}", command=command)
+            result[f"pi@{manager}: " + command] = r[0]['success']
 
             # add each worker hostname into manager exports file
             for worker in workers:
-                Console.info(f"Adding {worker} to manager exports")
                 add_to_exports = f"{mounting_to} {worker}(rw,no_root_squash,sync,no_subtree_check)"
-                r = Host.ssh(hosts=f"pi@{manager}",
-                             command=f"echo \"{add_to_exports}\" | sudo tee --append /etc/exports")
-                Console.info(r)
-            Host.ssh(hosts=f"pi@{manager}", command="sudo exportfs -r")
+                command = f"echo \"{add_to_exports}\" | sudo tee --append /etc/exports"
+                r = Host.ssh(hosts=f"pi@{manager}",command=command)
+                result[f"pi@{manager}: " + command] = r[0]['success']
+            
+            #restart NFS exports 
+            command = "sudo exportfs -r"
+            Host.ssh(hosts=f"pi@{manager}", command=command)
+            result[f"pi@{manager}: " + command] = r[0]['success']
 
-            # ssh into workers, mount directory
+
             for worker in workers:
-                Console.info(f'Setting up {worker}')
-                Console.info("mkdir /mnt/nfs worker")
-                r = Host.ssh(hosts=f"pi@{worker}", command=f"sudo mkdir -p {mounting_to}")
-                Console.info(r)
-                r = Host.ssh(hosts=f"pi@{worker}", command=f"sudo chown -R pi:pi {mounting_to}")
+                #create share point on workers
+                _create_share_system(worker,mounting_to)
 
-                # The *** is not use in cloudmesh bad style we habve banner to empasize things
-                Console.info('*****ATTEMPTING MOUNT******')
-                r = Host.ssh(hosts=f"pi@{worker}", command=f"sudo mount -vvvv {manager_ip}:{mounting_to} {mounting_to}")
-                Console.info(r)
-                Console.info("edit fstab worker")
+                #mount worker share point to manager share point 
+                command = f"sudo mount {manager_ip}:{mounting_to} {mounting_to}"
+                r = Host.ssh(hosts=f"pi@{worker}", command=command)
+                result[f"pi@{worker}: " + command] = r[0]['success']
+
+                #preserve mount after reboot on worker
                 add_to_fstab = f"{manager_ip}:{mounting_to}\t{mounting_to}\tnfs\tx-systemd.automount\t0\t0"
-                r = Host.ssh(hosts=f"pi@{worker}", command=f"echo \"{add_to_fstab}\" | sudo tee --append  /etc/fstab")
-                Console.info(r)
+                command = f"echo \"{add_to_fstab}\" | sudo tee --append  /etc/fstab"
+                r = Host.ssh(hosts=f"pi@{worker}", command=command)
+                result[f"pi@{worker}: " + command] = r[0]['success']
+
+            for key,value in result.items():
+                Console.info(f"{key} --> {value}")
+            result.clear()
 
         except AttributeError as e:
             Console.error("Not enough hostnames")
@@ -97,10 +109,17 @@ class Nfs:
             pass
 
     def unshare(self, path, hostnames, terminate=True):
+
+        #for debugging
         result = {}
         
+        #unmount and delete share point, remove reboot instructions
         def _unshare(host,path):
-            command=f"sudo umount -l {path}"
+            command=f"sudo umount -f {path}"
+            r = Host.ssh(hosts=f"pi@{host}", command=command)
+            result[f"pi@{host}: " + command] = r[0]['success']
+
+            command = f"sudo rm -r {path}"
             r = Host.ssh(hosts=f"pi@{host}", command=command)
             result[f"pi@{host}: " + command] = r[0]['success']
 
@@ -115,19 +134,20 @@ class Nfs:
             r = Host.ssh(hosts=f"pi@{host}", command=command)
             result[f"pi@{host}: " + command] = r[0]['success']
         
+        #necessary hostnames for unsharing
         pis = hostnames.split(',')
         manager = pis[0]
         workers = pis[1:]
 
+        #remove share point on workers
         for worker in workers:
             _unshare(worker,path)
 
-        # For each worker pi entered, we remove permissions for workers from the MANAGER'S /etc/exports file
-        # print("removing permissions for workers in /etc/exports")
+        #remove worker access permissions for manager share point
         command = f"cat /etc/exports"
         r = Host.ssh(hosts=f"pi@{manager}", command=command)
-        exports_file_text = r[0]['stdout']
         result[f"pi@{manager}: " + command] = r[0]['success']
+        exports_file_text = r[0]['stdout']
         lines = exports_file_text.splitlines()
 
         for worker in workers:
@@ -138,10 +158,11 @@ class Nfs:
         r = Host.ssh(f"pi@{manager}", command=command)
         result[f"pi@{manager}: " + command] = r[0]['success']
 
-        # if manager is included in hostnames, then we will be unmounting its shared drive
-        # (We do not want it shared with anyone, so no need to keep it mounted)
+        # unmount and remove manager share point, if specified 
+        # otherwise we may want to remove workers but still keep the manager share point open
         if terminate:
             _unshare(manager,path)
         
         for key,value in result.items():
             Console.info(f"{key} --> {value}")
+        result.clear()
