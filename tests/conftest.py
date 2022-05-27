56eb19356a339883f8ae8c8d3faea9290965fc6f
J.P <stapmoshun@gmail.com>
2022-05-27 15:30:22 -0400

    pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.name
+    option_value = metafunc.config.option.username
     if 'username' in metafunc.fixturenames and option_value is not None:
         metafunc.parametrize("username", [option_value])
     if 'hostname' in metafunc.fixturenames and option_value is not None:
